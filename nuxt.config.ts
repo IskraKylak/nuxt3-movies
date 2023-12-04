c280af9a477945bb889c13b037a6a36ad219b34a
Vlad <kmlider001@gmail.com>
2023-12-04 02:44:51 +0200

    commit18

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,10 @@ export default defineNuxtConfig({
     '@vueuse/nuxt'
   ],
   runtimeConfig: {
-    apiKey: '',
-    apiBaseUrl: '',
+    apiKey: process.env.NUXT_API_KEY || '',
+    apiBaseUrl: process.env.NUXT_API_BASE_URL || '',
     public: {
-      imgBaseUrl: ''
+      imgBaseUrl: process.env.NUXT_PUBLIC_IMG_BASE_URL || ''
     }
-  }
+  },
 })
